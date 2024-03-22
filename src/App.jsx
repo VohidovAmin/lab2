1470c31fae795b608a7947a56be8bfa16950706a
= <=>
2024-03-22 06:47:57 +0300

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import Button from './button'
 import Conteiner from './conteiner'
+import Header from './Header'
+import Menu from './Menu'
+import Footer from './Footer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,31 +13,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Header>
+        Мои лабораторные работы!
+        </Header>
+        <Menu/>
       <Conteiner> 
       <Button onClick={()=>alert("мой текст")} label={"Увеличить"}/>
       <Button onClick={()=>alert("мой код")} label={"Уменьшить"}/>
       <Button onClick={()=>alert("моя страница")} label={"Умножить"}/>
       </Conteiner>
+      <Footer>
+      Вохидов А.А
+      </Footer>
     </>
   )
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer({children}) {
+    return (
+        <footer>
+            <h1>{children}</h1>
+        </footer>
+    )
+}
+
+  export default Footer
